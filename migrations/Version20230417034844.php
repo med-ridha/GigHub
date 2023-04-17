8c6c476209d308fe5ce8b53accef51810d902b8b
med-ridha <ridha.zemzem24@gmail.com>
2023-04-17 05:26:59 +0100

    firs commit, best commit

---
+++ migrations/Version20230417034844.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230417034844 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD fullname VARCHAR(255) NOT NULL, ADD phonenumber VARCHAR(255) NOT NULL, ADD age INT NOT NULL, ADD gender VARCHAR(255) NOT NULL, CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP fullname, DROP phonenumber, DROP age, DROP gender, CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(Request $request): Response
+    {
+        $name = $request->query->get('name');
+        return $this->render('home/home.html.twig', [
+            'name' => $name,
+        ]);
+    }
+}
